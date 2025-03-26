66c60a7d671d073c3653fa90d8d360b6884b4a00
eric-sanchezz <ericsaanchezzz@gmail.com>
2025-03-26 18:10:53 +0100

    ex1

--- M0487_RA2_Pt1_Sanchez_Eric/pt1_act1.py
+++ M0487_RA2_Pt1_Sanchez_Eric/pt1_act1.py
@@ -1,5 +1,12 @@
 print ("Hola, Python!")
 print ("Èric Sánchez Velasco")
+
+# Elimineu les cometes dobles 
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+# Elimineu els parentesis
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
 a = "Benvingut"
 b = "al món de Python!"
 print(a, b)
